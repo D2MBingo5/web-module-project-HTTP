4df90b7ae98b00b3789d5c7310a91071b70cb6e6
Leu Family <romanianfobjr@gmail.com>
2021-08-18 16:23:56 -0500

    edit functionality complete

--- client/src/App.js
+++ client/src/App.js
@@ -45,7 +45,7 @@ const App = (props) => {
         
           <Switch>
             <Route path="/movies/edit/:id">
-              <EditMovieForm />
+              <EditMovieForm setMovie={setMovies} />
             </Route>
 
             <Route path="/movies/:id">
--- client/src/components/EditMovieForm.js
+++ client/src/components/EditMovieForm.js
@@ -33,6 +33,12 @@ const EditMovieForm = (props) => {
 
     const handleSubmit = (e) => {
 		e.preventDefault();
+		axios.put(`http://localhost:5000/api/movies/${id}`, movie)
+			.then(res => {
+				props.setMovie(res.data)
+				push(`/movies/${id}`)
+			})
+			.catch(err => {console.log(err)})
 	}
 	
 	const { title, director, genre, metascore, description } = movie;
