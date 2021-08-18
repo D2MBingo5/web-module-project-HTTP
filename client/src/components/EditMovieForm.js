2662001feca02eb7e739988d2e17b0748a066171
Leu Family <romanianfobjr@gmail.com>
2021-08-18 15:55:25 -0500

    id grabbed with useParams

--- client/src/components/EditMovieForm.js
+++ client/src/components/EditMovieForm.js
@@ -6,6 +6,7 @@ import axios from 'axios';
 
 const EditMovieForm = (props) => {
 	const { push } = useHistory();
+	const { id } = useParams()
 
 	const [movie, setMovie] = useState({
 		title:"",
@@ -21,6 +22,14 @@ const EditMovieForm = (props) => {
             [e.target.name]: e.target.value
         });
     }
+
+	useEffect(() => {
+		axios.get(`http://localhost:5000/api/movies/${id}`)
+			.then(res => {
+				setMovie(res.data)
+			})
+			.catch(err => console.log(err))
+	}, [])
 
     const handleSubmit = (e) => {
 		e.preventDefault();
