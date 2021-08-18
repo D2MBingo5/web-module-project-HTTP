b3ae26184c89f3fb2e742e30b0d0804d3f7f228b
Leu Family <romanianfobjr@gmail.com>
2021-08-18 17:34:50 -0500

    add movie form created with edit movie form as base

--- client/src/components/AddMovieForm.js
+++ client/src/components/AddMovieForm.js
@@ -1,10 +1,61 @@
 import React, { useState, useEffect } from "react"
 import axios from 'axios'
 
+import { Link } from 'react-router-dom'
+
 const AddMovieForm = (props) => {
 
+    const [movie, setMovie] = useState({
+        title: '',
+        director: '',
+        genre: '',
+        metascore: 0,
+        description: ''
+    })
+
+    const { title, director, genre, metascore, description } = movie
+
     return (
-        <div>ADD MOVIE FORM TEST</div>
+        <div>
+            <div className='col'>
+                <div className='modal-content'>
+                    {/* add components in all nulls */}
+                    <form onSubmit={null}>
+                        <div className='modal-header'>
+                            <h4 className='modal-title'>Add Movie</h4>
+                        </div>
+                        <div className='modal-body'>
+                            <div className='form-group'>
+                                <label>Title</label>
+                                <input value={title} onChange={null} name='title' type='text' className='form-control'/>
+                            </div>
+                            <div className='form-group'>
+                                <label>Director</label>
+                                <input value={director} onChange={null} name='director' type='text' className='form-control' />
+                            </div>
+                            <div className='form-group'>
+                                <label>Genre</label>
+                                <input value={genre} onChange={null} name='genre' type='text' className='form-control'/>
+                            </div>
+                            <div className='form-group'>
+                                <label>Metascore</label>
+                                <input value={metascore} onChange={null} type='number' className='form-control'/>
+                            </div>
+                            <div className='form-group'>
+                                <label>Description</label>
+                                <textarea value={description} onChange={null} name='description' className='form-control'></textarea>
+                            </div>
+                        </div>
+                        <div className='modal-footer'>
+                            <input type='submit' className='btn btn-info' value='Add'/>
+                            <Link to={`/movies`}>
+                                <input type='button' className='btn btn-default' value='Cancel' />
+                            </Link>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>        
     )
 }
 
